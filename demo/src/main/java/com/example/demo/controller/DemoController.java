5fc81352a850827e0df15b21a177d0d0591ab4f1
Richard Tang <Richard_L_Tang@mckinsey.com>
2023-04-07 10:40:11 +0700

    init project

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello";
+    }
+
+}
